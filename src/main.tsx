80bbd5c152b73dd507a460106b29b6aeba425993
mundodigitas45-ai <mundodigitas45@gmail.com>
2026-08-11 23:04:46 -0300

    load product media styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+import "./product-media.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
